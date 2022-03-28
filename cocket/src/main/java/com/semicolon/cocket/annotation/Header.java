8ebc67cca98804b37f2aa4fb1eb16037befc4df8
shw0471 <shw0471@naver.com>
2022-03-28 19:05:17 +0900

    add @Header annotation

---
+++ cocket/src/main/java/com/semicolon/cocket/annotation/Header.java
@@ -0,0 +1,13 @@
+package com.semicolon.cocket.annotation;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Target(PARAMETER)
+@Retention(RUNTIME)
+public @interface Header {
+    String value() default "";
+}
